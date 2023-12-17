55672dc7c39933f07183f428d1fc1a8897749f4a
Léon Zeitler <98434601+ZeitDev@users.noreply.github.com>
2023-12-17 18:28:31 +0100

    Add files via upload

--- zeitler/new_paper.py
+++ zeitler/new_paper.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from collections import Counter
 from matplotlib import pyplot as plt
 
+import pywt
 import scipy
 import torch
 import skimage
@@ -45,53 +46,71 @@ class Dataset(torch.utils.data.Dataset):
         return image, label, name
 
 def feature_extraction(image):
-    image = skimage.filters.unsharp_mask(image, radius=3, amount=7)
+    image_sharpened = skimage.filters.unsharp_mask(image, radius=0.5, amount=2)
 
-    image_rgb = (image*255).astype('uint8')
-    image_gray = (skimage.color.rgb2gray(image_rgb) * 255).astype('uint8')
+    image_rgb = image_sharpened #(image_sharpened * 255).astype('uint8')
+    #image_gray = (skimage.color.rgb2gray(image_rgb) * 255).astype('uint8')
 
-    mean = np.mean(image_rgb)
-    standard_deviation = np.std(image_rgb)
-    median = np.median(image_rgb)
-    percentile_25 = np.percentile(image_rgb, 25)
-    percentile_75 = np.percentile(image_rgb, 75)
-    kurtosis = scipy.stats.kurtosis(image_rgb.flatten())
+    image_red = image_rgb[:, :, 0]
+    image_green = image_rgb[:, :, 1]
+    image_blue = image_rgb[:, :, 2]
 
-    glcm = skimage.feature.graycomatrix(image_gray, [1], [0, np.pi/4, 2*np.pi/4, 3*np.pi/4])
-    contrast = skimage.feature.graycoprops(glcm, 'contrast')
-    dissimilarity = skimage.feature.graycoprops(glcm, 'dissimilarity')
-    homogeneity = skimage.feature.graycoprops(glcm, 'homogeneity')
-    energy = skimage.feature.graycoprops(glcm, 'energy')
-    correlation = skimage.feature.graycoprops(glcm, 'correlation')
-    asm = skimage.feature.graycoprops(glcm, 'ASM')
+    # 2D-DFT
+    dft_shift_red = np.fft.fftshift(np.fft.fft2(image_red))
+    dft_shift_green = np.fft.fftshift(np.fft.fft2(image_green))
 
-    _, image_thresh = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY)
-    moments = cv2.moments(image_thresh)
-    hu_moments = cv2.HuMoments(moments)
+    magnitude_spectrum_red = 20 * np.log(np.abs(dft_shift_red))
+    magnitude_spectrum_green = 20 * np.log(np.abs(dft_shift_green))
 
-    return {'mean': mean, 'standard_deviation': standard_deviation, 'median': median, 'percentile_25': percentile_25, 'percentile_75': percentile_75, 'kurtosis': kurtosis, 
-            'contrast_0': contrast[0][0], 'contrast_1': contrast[0][1], 'contrast_2': contrast[0][2], 'contrast_3': contrast[0][3],
-            'dissimilarity_0': dissimilarity[0][0], 'dissimilarity_1': dissimilarity[0][1], 'dissimilarity_2': dissimilarity[0][2], 'dissimilarity_3': dissimilarity[0][3],
-            'homogeneity_0': homogeneity[0][0], 'homogeneity_1': homogeneity[0][1], 'homogeneity_2': homogeneity[0][2], 'homogeneity_3': homogeneity[0][3],
-            'energy_0': energy[0][0], 'energy_1': energy[0][1], 'energy_2': energy[0][2], 'energy_3': energy[0][3],
-            'correlation_0': correlation[0][0], 'correlation_1': correlation[0][1], 'correlation_2': correlation[0][2], 'correlation_3': correlation[0][3],
-            'asm_0': asm[0][0], 'asm_1': asm[0][1], 'asm_2': asm[0][2], 'asm_3': asm[0][3],
-            'hu_moments_0': hu_moments[0][0], 'hu_moments_1': hu_moments[1][0], 'hu_moments_2': hu_moments[2][0], 'hu_moments_3': hu_moments[3][0], 'hu_moments_4': hu_moments[4][0], 'hu_moments_5': hu_moments[5][0], 'hu_moments_6': hu_moments[6][0],
-            }
+    magnitude_spectrum_red = magnitude_spectrum_red / np.max(magnitude_spectrum_red)
+    magnitude_spectrum_green = magnitude_spectrum_green / np.max(magnitude_spectrum_green)
+
+    dft_red = skimage.transform.resize(magnitude_spectrum_red, (64, 64))
+    dft_green = skimage.transform.resize(magnitude_spectrum_green, (64, 64))
+
+    # 2D-DWT
+    _, (cH, _, _) = pywt.dwt2(image_rgb, 'haar')
+    cH_red = skimage.transform.resize(cH[:, :, 0], (64, 64))
+    cH_green = skimage.transform.resize(cH[:, :, 1], (64, 64))
+
+    # feature normalization
+    # dft_red = z_score_normalization(dft_red.flatten())
+    # dft_green = z_score_normalization(dft_green.flatten())
+    # cH_red = z_score_normalization(cH_red.flatten())
+    # cH_green = z_score_normalization(cH_green.flatten())
+
+    # features_combined = np.concatenate([dft_red, dft_green, cH_red, cH_green])
+
+    return {'dft_red': dft_red.flatten(), 'dft_green': dft_green.flatten(), 'cH_red': cH_red.flatten(), 'cH_green': cH_green.flatten()}
 
 def create_dataset(data_path, test=False):
-    dataset = {'names': [], 'images': [], 'labels': [], 'features': {}, 'metrics': {}}
+    dataset = {'names': [], 'labels': [], 'features': {}, 'metrics': {}}
     for image, label, name in tqdm(Dataset(data_path)):
         dataset['names'].append(name)
-        dataset['images'].append(image)
         if not test: dataset['labels'].append(int(label))
         features = feature_extraction(image)
         for feature in features:
-            if feature in dataset['features']: dataset['features'][feature].append(features[feature])
-            else: dataset['features'][feature] = [features[feature]]
+            if feature in dataset['features']: np.append(dataset['features'][feature], features[feature])
+            else: dataset['features'][feature] = features[feature]
         
-        if len(dataset['images']) == 500: break
-        #if len(dataset['images']) == 200: break
+        if len(dataset['names']) == 500: break
+        #if len(dataset['names']) == 250: break
+        #if len(dataset['names']) == 20: break
+
+    return dataset
+
+def normalize_dataset(dataset):
+    for feature in dataset['features']:
+        dataset['features'][feature] = z_score_normalization(dataset['features'][feature].flatten())
+
+    for i in range(len(dataset['names'])):
+        features_combined = []
+        for feature in dataset['features']:
+            feature_list = list(dataset['features'][feature])
+            features_combined.append(feature_list[i])
+        dataset['features_combined'].append(features_combined)
+
+    dataset['features_combined'] = np.concatenate([dataset['features']['dft_red'], dataset['features']['dft_green'], dataset['features']['cH_red'], dataset['features']['cH_green']])
 
     return dataset
 
@@ -101,44 +120,6 @@ def z_score_normalization(values):
     normalized_values = (values - mean_value) / std_deviation
 
     return normalized_values
-
-def normalize_dataset(dataset):
-    if do_z_score_normalization:
-        for feature in dataset['features']:
-            dataset['features'][feature] = z_score_normalization(dataset['features'][feature])
-
-    dataset['features_combined'] = []
-    for i in range(len(dataset['images'])):
-        features_combined = []
-        for feature in dataset['features']:
-            features_combined.append(dataset['features'][feature][i])
-        dataset['features_combined'].append(features_combined)
-
-    if do_unit_normalization:
-        for i in range(len(dataset['features_combined'])):
-            feature_vector = np.array(dataset['features_combined'][i])
-            dataset['features_combined'][i] = list(feature_vector / np.linalg.norm(feature_vector))
-
-    return dataset
-
-def get_vector_features(dataset):
-    for i in tqdm(range(len(dataset['images']))):
-        image = dataset['images'][i]
-        image_rgb = (skimage.filters.unsharp_mask(image, radius=3, amount=7) * 255).astype('uint8')
-        image_gray = (skimage.color.rgb2gray(image_rgb) * 255).astype('uint8')
-        
-        # Color Histogram
-        hist = cv2.calcHist([image_rgb], [0, 1, 2], None, [32, 32, 32], [0, 256, 0, 256, 0, 256])
-        hist = hist / hist.sum()
-
-        # Edge Detection
-        edges = cv2.Canny(image_gray, 50, 150)
-        normalized_edges = edges / 255.0
-
-        features = list(hist.flatten()) + list(normalized_edges.flatten())
-        dataset['features_combined'][i].extend(list(features))
-
-    return dataset
 
 def get_metrics(labels, predictions):
     accuracy = round(100*accuracy_score(labels, predictions))
@@ -164,10 +145,7 @@ def get_metrics(labels, predictions):
     return accuracy, f1, recall, precision, splitted_metrics
 
 # %% SETTINGS
-do_z_score_normalization = True
-do_unit_normalization = False
-
-do_SVM = False
+do_SVM = True
 do_RF = True
 do_XGB = True
 do_LDA = True
@@ -183,12 +161,7 @@ sampling = False
 if 'dataset' not in locals():
     dataset = create_dataset(os.path.join('ISM', 'train'))
     dataset = normalize_dataset(dataset)
-    dataset = get_vector_features(dataset)
-
     dataset['labels'] = np.array(dataset['labels'])
-    dataset['features_combined'] = np.array(dataset['features_combined'])
-    del dataset['images']
-    del dataset['features']
 
     if do_pca:
         from sklearn.decomposition import PCA
